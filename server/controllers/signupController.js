96eec80d4051038c4d83598e3a1162a80b0d894d
Truman Ngo <83289775+truman2cool@users.noreply.github.com>
2022-10-17 20:04:07 -0400

    installed express-session

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -3,10 +3,15 @@ const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) =>{
   const {user, pwd}= req.body;
+  //check fields
   if(!user || !pwd) return res.status(400).json({"message":"email and password are required"})
   // check for duplicate usernames in the db
   const duplicate = await user.findOne({email: user}).exec();
   if(duplicate) return res.sendStatus(409); //conflict
+  //check pw length
+  if(pwd.length < 6){
+    return res.status(400).json({ msg: "Password should be atleast 6 characters long" });
+  }
   try{
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+//const session = require("express-session");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
@@ -14,7 +15,24 @@ app.use(require("./routes/user"));
 
 // get driver connection
 const dbo = require("./db/conn");
- 
+
+/*const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
+// Express-Session
+app.use(
+  session({
+    name: "localhost:5000", //name to be put in "key" field in postman etc
+    secret: "secret",
+    resave: true,
+    saveUninitialized: false,
+    store: dbo,
+    cookie: {
+      maxAge: MAX_AGE,
+      sameSite: false,
+      secure: true
+    }
+  })
+);*/
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
