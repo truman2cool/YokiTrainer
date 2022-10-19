b69055d321b153c439a66979bdedc810dad8e74f
Truman Ngo <83289775+truman2cool@users.noreply.github.com>
2022-10-19 07:27:31 -0400

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./css/App.css";
 // We use Route in order to define the different routes of our application
 import { Route, Routes} from "react-router-dom";
-
+//import { Provider } from 'react-redux';
+//import store from './store';
 // We import all the components we need in our app
 import Home from "./pages/home";
 import Landing from "./pages/landing";
@@ -16,10 +17,11 @@ import Menu from "./pages/menu";
 import About from "./pages/about";
 
 
-function App () {
+
+/*class App extends Component {
+  render(){
  return (
-   <div className="App"> 
-      <div className="pages">  
+      <Provider >  
       <Navbar />
         <Routes>      
           <Route exact path="/" element={<Home />} />
@@ -32,9 +34,29 @@ function App () {
           <Route path="/create" element={<Create />} /> 
           <Route path="/about" element={<About />} />
         </Routes>
-      </div> 
-   </div>
+      </Provider> 
  );
-}
+}};*/
+
+
+
+  function App () {
+   return (
+     <div className="App">   
+        <Navbar />
+        <Routes>      
+          <Route exact path="/" element={<Home />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/userpage" element={<Userpage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login /> } />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/create" element={<Create />} /> 
+          <Route path="/about" element={<About />} />
+        </Routes>
+     </div>
+   );
+  };
  
 export default App;
