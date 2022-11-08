858839743209ef0f1657f269f6ee7895b7636435
Truman Ngo <83289775+truman2cool@users.noreply.github.com>
2022-11-08 17:45:04 -0500

    File clean up
    deleted useless files

--- client/src/App.js
+++ client/src/App.js
@@ -10,22 +10,12 @@ import Edit from "./components/edit";
 import Create from "./components/create";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-//import Navbar from "./components/navbar";
 import Menu from "./pages/menu";
 import About from "./pages/about";
 import Logout from "./pages/logout";
 import Test from "./pages/Test";
 import CreateTest from "./components/createTest";
-//import Layout from "./components/Layout";
-//import useAuth from "./hooks/useAuth";
-//import Unauthorized from "./components/Unauthorized";
-//import RequireAuth from "./components/RequireAuth"
 
-/*const ROLES = {
-  'User': 2001,
-  'Editor': 1984,
-  'Admin': 5150
-}*/
 
 function App (){
 
@@ -46,43 +36,5 @@ function App (){
    </Routes>
      </div>
    )};
-
-   /*return (
-  <Routes>
-    <Route path="/" element={<Layout />}>  
-        {/*public*/
-    /*<Route path="/" element={<Home />} />    
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/signup" element={<Signup />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/Unauthorized" element={<Unauthorized />} />
-
-    <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/logout" element={<Logout />} />
-        </Route>
-
-    <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/edit:id" element={<Edit />} />
-        </Route>
-
-
-    <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/menu" element={<Menu />} />
-        </Route>
-
-
-    <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/create" element={<Create />} />
-        </Route>
-
-
-    </Route>
-  </Routes>
-
-  )};*/
 
 export default App;
